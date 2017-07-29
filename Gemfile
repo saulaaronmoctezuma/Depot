7baf2e0342808b7181daef35c18fc3624977aff2
saulaaronmoctezuma <saulaaronmoctezuma@hotmail.com>
2017-07-28 22:42:44 -0500

    'Capitulo11'

--- Gemfile
+++ Gemfile
@@ -5,7 +5,8 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
-
+gem 'jquery-rails'
+gem 'jquery-ui-rails'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.2'
 # Use sqlite3 as the database for Active Record
@@ -21,7 +22,7 @@ gem 'uglifier', '>= 1.3.0'
 
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
-# Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
+#w Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -39,17 +39,23 @@ class ProductsController < ApplicationController
 
   # PATCH/PUT /products/1
   # PATCH/PUT /products/1.json
-  def update
-    respond_to do |format|
-      if @product.update(product_params)
-        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
-        format.json { render :show, status: :ok, location: @product }
-      else
-        format.html { render :edit }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+ def update
+respond_to do |format|
+if @product.update(product_params)
+format.html { redirect_to @product,
+notice: 'Product was successfully updated.' }
+format.json { render :show, status: :ok, location: @product }
+
+ @products = Product.all
+ ActionCable.server.broadcast 'products',
+ html: render_to_string('store/index', layout: false)
+else
+format.html { render :edit }
+format.json { render json: @product.errors,
+status: :unprocessable_entity }
+end
+end
+end
 
   # DELETE /products/1
   # DELETE /products/1.json
